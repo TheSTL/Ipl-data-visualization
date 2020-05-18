b80565f91a7665cc43f24381b0d27bd755769b64
ashu <ashudeshwal999@gmail.com>
2020-05-19 00:07:22 +0530

    import data dynamically to increase page load

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "../../node_modules/react-vis/dist/style.css";
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/core";
 import MatchData from "../data/match.json";
-import BallData from "../data/ball.json";
 import MatchPlayedAt from "../components/MatchPlayedAt/MatchPlayedAt";
 import MatchHisotry from "../components/MatchHisotry/MatchHistory";
 import { getDataFromMatchJson, getDataFromBallJson } from "../utils";
@@ -23,19 +22,36 @@ class Home extends React.Component {
 
   componentDidMount() {
     window.addEventListener("resize", this.updateSize);
-
     const matchDataExtracted = getDataFromMatchJson(MatchData);
-    const ballDataExtracted = getDataFromBallJson(BallData);
 
     this.setState({
       ...matchDataExtracted,
-      ...ballDataExtracted,
       isLoading: false,
     });
   }
+
   componentWillMount() {
     window.removeEventListener("resize", this.updateSize);
   }
+
+  importBallToBallData = () => {
+    const { matchDetails } = this.state;
+    if (!Object.keys(matchDetails).length) {
+      this.setState({
+        isLoading: true,
+      });
+
+      import("../data/ball.json").then(({ default: ballData }) => {
+        const ballDataExtracted = getDataFromBallJson(ballData);
+
+        this.setState({
+          ...ballDataExtracted,
+          isLoading: false,
+        });
+        return ballData;
+      });
+    }
+  };
 
   updateSize = () => {
     this.setState({
@@ -55,6 +71,15 @@ class Home extends React.Component {
     };
   };
 
+  renderLoadingScreen = () => {
+    return (
+      <div className="loading-screen">
+        <h1>Loading please wait... </h1>
+        <img src="/loading.gif" alt="" />
+      </div>
+    );
+  };
+
   render() {
     const {
       matchPerVenue,
@@ -67,34 +92,22 @@ class Home extends React.Component {
       isLoading,
     } = this.state;
 
-    if (isLoading) {
-      return (
-        <div className="loading-screen">
-          <h1>Loading please wait... </h1>
-        <img  src='/loading.gif' alt=""/>
-        </div>
-      )
-    }
-
     return (
       <div className="home-page">
         <h1>IPL Visualization</h1>
         <Tabs isFitted variant="enclosed" style={{ width: "100%" }}>
           <TabList mb="1em">
-            <Tab _selected={{ color: "white", bg: "blue.500" }}>Matches</Tab>
             <Tab _selected={{ color: "white", bg: "green.500" }}>
               Matches Played at different location
             </Tab>
+            <Tab
+              onClick={this.importBallToBallData}
+              _selected={{ color: "white", bg: "blue.500" }}
+            >
+              Matches
+            </Tab>
           </TabList>
           <TabPanels>
-            <TabPanel>
-              <MatchHisotry
-                matchDetails={matchDetails}
-                matchExtraDetails={matchExtraDetails}
-                visWidth={visWidth}
-                visHeight={visHeight}
-              />
-            </TabPanel>
             <TabPanel>
               <MatchPlayedAt
                 matchPerVenue={matchPerVenue}
@@ -103,6 +116,18 @@ class Home extends React.Component {
                 visWidth={visWidth}
                 visHeight={visHeight}
               />
+            </TabPanel>
+            <TabPanel>
+              {isLoading ? (
+                this.renderLoadingScreen()
+              ) : (
+                <MatchHisotry
+                  matchDetails={matchDetails}
+                  matchExtraDetails={matchExtraDetails}
+                  visWidth={visWidth}
+                  visHeight={visHeight}
+                />
+              )}
             </TabPanel>
           </TabPanels>
         </Tabs>
